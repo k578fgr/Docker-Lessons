182bbf81441f7d0679a64b3566dc342f18d86afb
Kirill <jeff31011999@gmail.com>
2022-12-12 22:12:29 +0300

    docker file

---
+++ Baikal-proekt/app.module.ts
@@ -0,0 +1,16 @@
+import { CacheModule, Module } from '@nestjs/common';
+import * as redisStore from 'cache-manager-redis-store';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+    }),
+  ],
+})
+export class AppModule {}
